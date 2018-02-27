590103a5945f6ff0ab2ac372eefb944a95c64450
Nicolas Ledez <github@ledez.net>
2018-02-27 21:52:31 +0100

    Add example

---
+++ unseal.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from unseal_vault import get_vault_server, get_config, unseal
+import socket
+import socks
+import urllib3
+
+# Disable ssl warning:
+urllib3.disable_warnings()
+
+# Use proxy socks:
+socks.set_default_proxy(socks.SOCKS5, '127.0.0.1', 8585)
+socket.socket = socks.socksocket
+
+vault_servers = get_vault_server('vault')
+config = get_config('passtore', 'vault-test')
+config = get_config('yaml', 'tests/vault-test.yaml')
+config = get_config('yaml', 'tests/vault-bad-test.yaml')
+for server in vault_servers:
+    unseal(server['address'],
+           server['port'],
+           config,
+           server['node_name'])
--- unseal_vault/__init__.py
+++ unseal_vault/__init__.py
@@ -12,14 +12,6 @@ import yaml
 import urllib3
 import sys
 
-# Disable ssl warning:
-urllib3.disable_warnings()
-
-# Use proxy socks:
-socks.set_default_proxy(socks.SOCKS5, '127.0.0.1', 8585)
-socket.socket = socks.socksocket
-
-
 def get_config(config_type, value):
     '''
     Get config wrapper uncomment witch you want to use
@@ -90,15 +82,3 @@ def unseal(host, port, config, name=None):
             print('Server unsealed')
     else:
         print('Server unsealed')
-
-
-if __name__ == "__main__":
-    vault_servers = get_vault_server('vault')
-    # config = get_config('passtore', 'vault-test')
-    # config = get_config('yaml', 'tests/vault-test.yaml')
-    # config = get_config('yaml', 'tests/vault-bad-test.yaml')
-    # for server in vault_servers:
-    #     unseal(server['address'],
-    #            server['port'],
-    #            config,
-    #            server['node_name'])
