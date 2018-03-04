17c17158ab59d5565e564fd96f68ba557e5a5f99
Nicolas Ledez <github@ledez.net>
2018-03-04 14:20:31 +0100

    test_unseal_vault_config refactoring

--- tests/test_unseal_vault_config.py
+++ tests/test_unseal_vault_config.py
@@ -2,19 +2,25 @@
 Test unseal_vault.config
 '''
 
-import unseal_vault.config
-
 import mock
 import pytest
+
+import unseal_vault.config
 
 from . import mock_content
 
 
 def missing_config(filename):
+    '''
+    Mock missing config file
+    '''
     return False
 
 
 def test_missing_config_file(capsys):
+    '''
+    Test missing config file
+    '''
     patcher_exists = mock.patch('os.path.exists')
     mock_exists = patcher_exists.start()
     mock_exists.side_effect = missing_config
@@ -26,13 +32,16 @@ def test_missing_config_file(capsys):
 
 
 def exist_config(filename):
-    if filename == '~/.unseal_vault.yml':
-        return True
-    else:
-        return False
+    '''
+    Mock config file exist
+    '''
+    return filename == '~/.unseal_vault.yml'
 
 
 def test_bad_config_file(capsys):
+    '''
+    Test bad config yaml file
+    '''
     patcher_exists = mock.patch('os.path.exists')
     mock_exists = patcher_exists.start()
     mock_exists.side_effect = exist_config
@@ -41,16 +50,21 @@ def test_bad_config_file(capsys):
 
     with mock.patch('unseal_vault.config.open',
                     mock.mock_open(read_data=config_content),
-                    create=True) as m:
+                    create=True) as m_open:
         with pytest.raises(SystemExit):
             unseal_vault.config.load()
-        m.assert_called_once_with(unseal_vault.config.CONFIG_PATH, 'r')
+        m_open.assert_called_once_with(unseal_vault.config.CONFIG_PATH, 'r')
         out, err = capsys.readouterr()
-        assert out.split('\n')[:-2] == mock_content.YAML_FILE_PARSE_ERROR.split('\n')[:-2]
+        out_without_filename = out.split('\n')[:-2]
+        assert_wo_fn = mock_content.YAML_FILE_PARSE_ERROR.split('\n')[:-2]
+        assert out_without_filename == assert_wo_fn
         print(out, err)
 
 
 def test_open_config_file():
+    '''
+    Test config yaml file
+    '''
     patcher_exists = mock.patch('os.path.exists')
     mock_exists = patcher_exists.start()
     mock_exists.side_effect = exist_config
@@ -59,26 +73,9 @@ def test_open_config_file():
 
     with mock.patch('unseal_vault.config.open',
                     mock.mock_open(read_data=config_content),
-                    create=True) as m:
+                    create=True) as m_open:
         config = unseal_vault.config.load()
-        m.assert_called_once_with(unseal_vault.config.CONFIG_PATH, 'r')
+        m_open.assert_called_once_with(unseal_vault.config.CONFIG_PATH, 'r')
         assert isinstance(config, dict)
         assert sorted(list(config.keys())) == ['vault-consul-passwdstore',
                                                'vault-yaml']
-
-
-def test_open_config_file():
-    patcher_exists = mock.patch('os.path.exists')
-    mock_exists = patcher_exists.start()
-    mock_exists.side_effect = exist_config
-
-    config_content = ''.join(open('tests/config.yml').readlines())
-
-    with mock.patch('unseal_vault.config.open',
-                    mock.mock_open(read_data=config_content),
-                    create=True) as m:
-        config = unseal_vault.config.load()
-        m.assert_called_once_with(unseal_vault.config.CONFIG_PATH, 'r')
-        assert isinstance(config, dict)
-        assert list(config.keys()) == ['vault-yaml',
-                                       'vault-consul-passwdstore']
